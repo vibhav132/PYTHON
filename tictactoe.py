f2acfc6937c6f15f76e3f9e25f32b35add1fefa5
vibhav132 <83224840+vibhav132@users.noreply.github.com>
2023-03-14 17:40:35 +0530

    modified tictoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,26 @@
+a = ["|","|",""]
+b = ["__","__","__"]
 class Matrix:
     def __init__(self) -> None:
         pass
+class Player():
+    def __init__(self,name):
+        self.name = name
+        self.wins = 0
+
+class Game:
+    def __init__(self):
+        name1 = input("PLAYER 1 NAME:")
+        name2 = input("PLAYER 2 NAME:")
+        self.player1 = Player(name1)
+        self.player2 = Player(name2)
+    def play_game(self):
+        self.player1.input = input("")
+for i in range(0,3):
+    for j in range(0,3):
+        if(i<2):
+          print(b[j],end="")
+        else:
+            print("  ",end="")
+        print(a[j],end="")
+    print()
